ce2754d6b8ee505d387a74a20926f1df3d3e469b
Vartika Yaduvanshi <vv1415@srmist.edu.in>
2022-01-03 22:51:06 +0530

    Clock

---
+++ Real_Time_Clock/index.js
@@ -0,0 +1,10 @@
+let a;
+let date;
+let time;
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+setInterval(() => {
+a= new Date();
+time = a.getHours() + `:` + a.getMinutes() + `:` + a.getSeconds() ;
+date = a.toLocaleDateString(undefined,options);
+document.getElementById('time').innerHTML=time + " on " + date;
+}, 1000);
